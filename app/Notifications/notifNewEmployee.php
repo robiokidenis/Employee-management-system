b33d191d66314902bbfce44d0ca98bb7a24b64ec
robiokidenis <robiokidenis@gmail.com>
2021-10-04 09:36:53 +0700

    Queue support

--- app/Notifications/notifNewEmployee.php
+++ app/Notifications/notifNewEmployee.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class notifNewEmployee extends Notification
+class notifNewEmployee extends Notification //implements ShouldQueue
 {
     use Queueable;
     protected $company;
